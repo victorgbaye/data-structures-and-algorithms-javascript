15c30f29ac103166d4125d9196e1fdbf6e552557
tomiwadotJS <victorgbayedev@gmail.com>
2023-07-13 09:19:21 +0100

    binary search tree insert node operation

--- DataStructures/BinarySearchTree/BinarySearchTree.js
+++ DataStructures/BinarySearchTree/BinarySearchTree.js
@@ -10,4 +10,29 @@ let BinaryTree = function(){
     this.isEmpty = function(){
         return this.root === null
     }
+    this.insert = function(value){
+        let newNode = new Node(value)
+        if(this.isEmpty){
+            return this.root = newNode
+        }else{
+            return this.insertNode(this.root, newNode)
+        }
+    }
+
+    this.insertNode = function(root, Node){
+        if(Node.value < root){
+            if(root.left === null){
+               root.left = Node
+            }else{
+                this.insertNode(root.left, Node)
+            }
+
+        }else{
+            if (root.right === null) {
+                root.right = Node
+            }else{
+                this.insertNode(root.right, Node)
+            }
+        }
+    }
 }
